a8f296f1d1116e68526c3f430a7996f31fd29477
kunghwan <ysw03031@naver.com>
2024-12-12 16:51:38 +0900

    second commit

---
+++ 24_12_11/오전/네이버/index.js
@@ -0,0 +1,10 @@
+
+//실행하고 싶은 동작
+function show (){
+    const modal = document.getElementById("modal");
+    modal.style = "display:flex";
+}
+function hide (){
+    const modal = document.getElementById("modal");
+    modal.style = "display:none";
+ }
